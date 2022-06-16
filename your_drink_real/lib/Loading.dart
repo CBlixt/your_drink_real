2d91ce520ad3bb90d0140e2ab849fb76d26d859b
Christina, s214946 <s214946@student.dtu.dk>
2022-06-16 13:48:33 +0200

    loading screen ændringer

--- your_drink_real/lib/Loading.dart
+++ your_drink_real/lib/Loading.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:your_drink_real/BrugerListe.dart';
 import 'package:your_drink_real/main.dart';
 
-class LoadingScreen extends StatelessWidget {
+class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState(){
+    super.initState();
+    navigateHome();
+  }
+
+  navigateHome() async{
+    await Future.delayed(Duration(milliseconds: 1500), () {});
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>BrugerListe()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.green[100],
       body: Center(
+
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+
           children: [
-            Text('Loading Screen'),
-            Image(image: AssetImage('assets/YourDrinkLogo.png'))
+
+            Image(image: AssetImage('assets/YourDrinkLogo.png'), height: 800,)
           ],
         ),
       ),
